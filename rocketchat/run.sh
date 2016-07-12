0cf198351d5f7abce60b8c326ad13ffa1d101fe7
Yusuke Hasegawa <yhasegaw@dhcp-193-54.nrt.redhat.com>
2016-07-12 16:43:40 +0900

    test

--- rocketchat/run.sh
+++ rocketchat/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-export MONGO_URL="mongodb://user:password@${MYDB_SERVICE_HOST}:27017/sampledb"
-export HOME=/tmp
-export PORT=3000
-export ROOT_URL=http://localhost:3000
-export Accounts_AvatarStorePath=/app/uploads
-export Accounts_UseDNSDomainCheck=false
+MONGO_URL="mongodb://user:password@${MYDB_SERVICE_HOST}:27017/sampledb"
+HOME=/tmp
+PORT=3000
+ROOT_URL=http://localhost:3000
+Accounts_AvatarStorePath=/app/uploads
+Accounts_UseDNSDomainCheck=false
 node main.js
 
 
