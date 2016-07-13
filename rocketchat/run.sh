072417d43a0408ccb20ba89e4cc10cf3f77a8d92
Yusuke Hasegawa <yhasegaw@dhcp-193-54.nrt.redhat.com>
2016-07-13 12:53:42 +0900

    test

--- rocketchat/run.sh
+++ rocketchat/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 MONGO_URL="mongodb://user:password@${MYDB_SERVICE_HOST}:27017/sampledb"
-HOME=/tmp
-PORT=3000
-ROOT_URL=http://localhost:3000
-Accounts_AvatarStorePath=/app/uploads
-Accounts_UseDNSDomainCheck=false
+#HOME=/tmp
+#PORT=3000
+#ROOT_URL=http://localhost:3000
+#Accounts_AvatarStorePath=/app/uploads
+#Accounts_UseDNSDomainCheck=false
 node main.js
 
 
